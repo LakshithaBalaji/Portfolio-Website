5d4352cbf7595bfcb9e8daf5cb6e6876c1f9496d
Lakshitha <lakshithabalajii20@gmail.com>
2024-05-24 11:11:44 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const circles= document.querySelectorAll('.circle');
+circles.forEach(elem=>{
+    var dots = elem.getAttribute("data-dots");
+    var marked = elem.getAttribute("data-percent");
+    var percent =Math.floor(dots*marked/100);
+    var points="";
+    var rotate=360 / dots;
+
+
+    for(let i=0;i<dots;i++){
+        points+=`<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
+    }
+    elem.innerHTML=points;
+    const pointsMarked=elem.querySelectorAll('.points');
+    for(let i=0;i<percent;i++){
+        pointsMarked[i].classList.add('marked')
+    }
+})
